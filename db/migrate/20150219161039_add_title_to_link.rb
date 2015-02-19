f75fd3075a9ae040050d8d4b2a8e3627b193adf0
Thomas Weld <thomas.weld@gmail.com>
2015-02-19 11:31:26 -0500

    $ rails generate model link

---
+++ db/migrate/20150219161039_add_title_to_link.rb
@@ -0,0 +1,9 @@
+class AddTitleToLink < ActiveRecord::Migration
+  def change
+
+
+  	add_column :links, :title, :string
+
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219155325) do
+ActiveRecord::Schema.define(version: 20150219161039) do
+
+  create_table "links", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "title"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.string   "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
